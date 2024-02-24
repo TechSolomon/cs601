22d8453b47732768ce35392f9b4713d112373851
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2024-02-23 15:45:45 -0900

    feat: sample template for LLVM IR

---
+++ playground/src/main.rs
@@ -0,0 +1,13 @@
+// main.rs
+// Solomon Himelbloom
+// 2024-02-23
+//
+// For CS 601 Spring 2024
+
+pub fn square(x: i32) -> i32 {
+    x * x
+}
+
+fn main() {
+    println!(">> LLVM -- Intermediate Representation (IR)");
+}
