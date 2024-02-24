a688f25f2f724db32d986a895198b4a46ea9f3b9
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2024-02-23 16:56:20 -0900

    feat: LLVM IR (module + version)

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -4,10 +4,16 @@
 //
 // For CS 601 Spring 2024
 
+use llvm_ir::Module;
+
+// https://mcyoung.xyz/2023/08/01/llvm-ir/#back-to-basic-blocks
 pub fn square(x: i32) -> i32 {
     x * x
 }
 
 fn main() {
-    println!(">> LLVM -- Intermediate Representation (IR)");
+    let _module = Module::from_bc_path("source.bc");
+    let version = llvm_ir::llvm_version();
+
+    println!(">> LLVM {} -- Intermediate Representation (IR)", version);
 }
---
+++ playground/src/source.c
@@ -0,0 +1,6 @@
+// https://github.com/olawlor/cs601/blob/master/orcjit/examples/input.c
+long jitentry() {
+  // printf("It works!\n");
+  int i = 3;
+  return i + i;
+}
